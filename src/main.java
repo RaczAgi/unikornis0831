27be950a84995b4eca49976854cbc25988213569
Ági <raczagi07@gmail.com>
2023-08-31 17:40:49 +0200

    min

--- src/main.java
+++ src/main.java
@@ -7,6 +7,18 @@ public class main {
         for (int i = 0; i < array.length; i++) {
             array[i] = random.nextInt(100);
         }
-
     }
+        public static int min(int[] array) throws Exception{
+        if (array.length == 0){
+            throw new IllegalArgumentException();
+        }
+            int min = array[0];
+            for (int i = 1; i < array.length; i++) {
+                if(array[i] < min){
+                    min = array[i];
+                }
+            } return min;
+        }
+
+
 }
