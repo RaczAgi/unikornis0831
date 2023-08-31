b0e4c626171c985a982db9da3b8c38819de8315f
Ági <raczagi07@gmail.com>
2023-08-31 17:31:34 +0200

    array

---
+++ src/main.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class main {
+    public static void main(String[] args) {
+        int[] array = new int[20];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+
+    }
+}
