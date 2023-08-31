8bfc504b4f5a24932955ac32a6567f48319d0788
Geree <smithzero94@gmail.com>
2023-08-31 17:56:12 +0200

    odd() method implemented

--- src/main.java
+++ src/main.java
@@ -22,28 +22,43 @@ public class main {
         return array[maxIndex];
     }
 
-    public static int min(int[] array) throws Exception{
-        if (array.length == 0){
+    public static int min(int[] array) throws Exception {
+        if (array.length == 0) {
             throw new IllegalArgumentException();
         }
-            int min = array[0];
-            for (int i = 1; i < array.length; i++) {
-                if(array[i] < min){
-                    min = array[i];
-                }
-            } return min;
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
 
+        return min;
     }
 
-    public static int even(int[] array) throws Exception{
-            if (array.length == 0){
-                throw new IllegalArgumentException();
+    public static int even(int[] array) throws Exception {
+        if (array.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] / 2 == 0) {
+                counter++;
             }
-            int counter = 0;
-            for (int i = 0; i < array.length; i++) {
-                if(array[i] / 2 == 0){
-                    counter++;
-                }
-            } return counter;
+        }
+        return counter;
+    }
+
+    public static int odd(int[] array) throws Exception {
+        if (array.length == 0) {
+            throw new NullPointerException();
+        }
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
